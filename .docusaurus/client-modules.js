58f0ff385443e4f6d3e0a3f8d57eed311f0d9910
Gitea <gitea@fake.local>
2020-11-15 16:11:51 +0530

    remove build

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/tharindu/Desktop/copper-plus/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/tharindu/Desktop/copper-plus/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/tharindu/Desktop/copper-plus/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/tharindu/Desktop/copper-plus/node_modules/infima/dist/css/default/default.css"),
-  require("/home/tharindu/Desktop/copper-plus/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/home/tharindu/Desktop/copper-plus/src/css/custom.css"),
-];
--- docs/prices.md
+++ docs/prices.md
@@ -2,3 +2,17 @@
 id: prices
 title: Prices
 ---
+
+### Your own social network. 
+
+Are you willing to run your **own social network** and connect with the copper network under your authority ? Yes it’s possible with copper-plus and please contact us with your thoughts. 
+
+***Email*** : hello@Legatorsolutions.com
+
+![img](../static/img/prices.svg)
+
+### Would you like to connect with us.
+
+Support to make a digitalize commodity market for Sri Lanka based on copper network. 
+
+***Email*** : hello@Legatorsolutions.com
